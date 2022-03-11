fe67c7cbd7642a663cb147bc4aceb6336b8a9636
Vsevolod <woornad@gmail.com>
2022-03-12 01:53:48 +0300

    Commit for merge request

---
+++ src/GridLib/CubeElem.h
@@ -0,0 +1,16 @@
+#ifndef __CUBE_ELEMENT__
+#define __CUBE_ELEMENT__
+
+class CubeElem
+{
+public :
+
+private:
+
+public:
+
+private:
+
+};
+
+#endif
---
+++ src/Math/VecLib.h
@@ -0,0 +1,12 @@
+#ifndef __VECTOR_OPERATIONS__
+#define __VECTOR_OPERATIONS__
+
+#include <vector>
+#include <cmath>
+
+template<typename T>
+std::vector<T> NumProduct(const std::vector<T> &, const double a);
+template<typename T>
+T DotProduct(const std::vector<T> &, const std::vector<T> &);
+
+#endif
